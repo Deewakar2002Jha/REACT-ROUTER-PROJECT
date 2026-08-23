4b55a15579b3e2bea98ffe00c96672faa541ce50
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-08-23 23:59:08 +0530

    new topic

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Homecss from "./pages/Homecss";
+import Aboutcss from "./pages/Aboutcss";
+import Contactcss from "./pages/Contactcss";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Homecss />} />
+        <Route path="/about" element={<Aboutcss />} />
+        <Route path="/contact" element={<Contactcss />} />
       </Routes>
 
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 };
